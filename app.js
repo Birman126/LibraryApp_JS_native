fcec9a2b168879a986af076ed79f3778c0029951
Алексей Бирев <birman126@gmail.com>
2022-05-09 11:58:18 +0400

    add delete button

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var books;
   ? (books = [])
   : (books = JSON.parse(localStorage.getItem("books")));
 
-
-
 function Book(title, description) {
   this.title = title;
   this.description = description;
@@ -17,7 +15,7 @@ for (var i = 0; i < radio.length; i++) {
 }
 
 function addForm() {
-  console.log(books);
+  
   var block = document.getElementById("radio-block");
   block.innerHTML = "";
   if (this.value == "create-book") {
@@ -32,31 +30,51 @@ function addForm() {
                 <button class="onload-book-btn" >ДОБАВИТЬ1</button>
                 `;
     block.innerHTML = newDiv.outerHTML;
+    const deskBookTitle = document.querySelector(".input-title-book");
+    const deskBookText = document.querySelector(".text-book");
+    const addBookBtn = document.querySelector(".onload-book-btn");
+    addBookBtn.onclick = function () {
+      books.push(new Book(deskBookTitle.value, deskBookText.value));
+      
+      updateLocal();
+      fillBookList();
+      createTemplate();
+    };
   } else if (this.value == "onload-book") {
     let newDiv = document.createElement("div");
     newDiv.className = "onload-book-form";
     newDiv.innerHTML = `
                 Название книги
-                <input class="input-title-book" type="text" />
+                <input class="input-file-title-book" type="text" />
                 <br />
-                <input type="file" class="file" />
-                <button class="onload-book-btn" >ДОБАВИТЬ2</button>
-              `;
+                <form action='#' id='form-file'>
+                <input type="file" id="file" />
+                <input type='submit' class="onload-book-file-btn">
+                </form>              
+                  `;
     block.innerHTML = newDiv.outerHTML;
-  }
-  const deskBookTitle = document.querySelector(".input-title-book");
-  const deskBookText = document.querySelector(".text-book");
-  const addBookBtn = document.querySelector(".onload-book-btn");
-
-  addBookBtn.onclick = function () {
-    books.push(new Book(deskBookTitle.value, deskBookText.value));
-    console.log(books);
-    updateLocal();
-    fillBookList();
-    createTemplate();
+    const addBookFileBtn = document.querySelector(".onload-book-file-btn");
+    const deskBookFileTitle = document.querySelector(".input-file-title-book");
     
-  };
+    
   
+    addBookFileBtn.onclick = function () {
+      const file = document.getElementById("file").files[0];
+      
+      let reader = new FileReader();
+      reader.onload = function () {
+        console.log(reader.result);
+        books.push(new Book(deskBookFileTitle.value, reader.result));
+      }
+      reader.readAsText(file);
+      console.log(reader.result);
+      // books.push(new Book(deskBookFileTitle.value, reader.result));
+      
+      updateLocal();
+      fillBookList();
+      createTemplate();
+    };
+  }
 }
 
 var bookList = document.querySelector(".book-list_all");
@@ -64,11 +82,9 @@ var bookList = document.querySelector(".book-list_all");
 const createTemplate = (books, index) => {
   return `
         <div class="book-box">
-            <p class="book-title">${books.title}</p>
-            <button>ред.</button>
-            <button>прочитал</button>
-            <button>читать</button>
-            <button>Х</button>
+            <p class="book-title">${books?.title}</p>
+            
+            <button onclick="deleteBook(${index})" class="btn-delete">Х</button>
         </div>
     `;
 };
@@ -76,36 +92,18 @@ const createTemplate = (books, index) => {
 const fillBookList = () => {
   bookList.innerHTML = "";
   if (books.length > 0) {
-    // for (let i=0; i < books.length; i++) {
-    //     bookList.innerHTML += 1
-    // }
     books.forEach((item, index) => {
       bookList.innerHTML += createTemplate(item, index);
     });
   }
 };
 
-
-
 const updateLocal = () => {
   localStorage.setItem("books", JSON.stringify(books));
 };
 
-
-
-// function addBookItem() {
-// var blockList = document.getElementById("book-list");
-// var newDivBookList = document.createElement("div");
-// newDivBookList.className = "book-list-item";
-// newDivBookList.innerHTML = `
-//     <div class="book-box">
-//       <p class="book-title">${books.title}</p>
-//       <button>ред.</button>
-//       <button>прочитал</button>
-//       <button>читать</button>
-//       <button>Х</button>
-//     </div>
-// `;
-// blockList.innerHTML = newDivBookList.outerHTML;
-// console.log(JSON.parse(localStorage.getItem("books")));
-// }
+const deleteBook = index => {
+  books.splice(index, 1);
+  updateLocal();
+  fillBookList();
+}
